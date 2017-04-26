3b5dab91a36f86cfed641a6549e0a3d105746805
Dr Von Martinez <DrVonMartinez@users.noreply.github.com>
2017-04-25 21:15:48 -0500

    Update LinkedU.sql
    fixed a lot of issues, it should build in netbeans now

--- LinkedU.sql
+++ LinkedU.sql
@@ -1,68 +1,68 @@
 CREATE TABLE LinkedU.Student
 (
-	firstName VARCHAR2(20) NOT NULL,
-	lastName VARCHAR2(20) NOT NULL,
-	userName VARCHAR2(10) NOT NULL,
-	highSchool VARCHAR2(40),
-	currentUniversity VARCHAR2(40),
-	universityChoices VARCHAR2(40),
-	majorChoices VARCHAR2(40),
-	essay VARCHAR2(40),
-	activities VARCHAR2(250),
+	firstName VARCHAR(20) NOT NULL,
+	lastName VARCHAR (20) NOT NULL,
+	userName VARCHAR(10) NOT NULL,
+	highSchool VARCHAR(40),
+	currentUniversity VARCHAR(40),
+	universityChoices VARCHAR(40),
+	majorChoices VARCHAR(40),
+	essay VARCHAR(40),
+	activities VARCHAR(250),
 	ACTScores INTEGER,
 	SATScores INTEGER,
 	GPA DOUBLE,
 	graduationDate DATE,
 	accountStatus BOOLEAN default false,
-	email VARCHAR2(35) NOT NULL,
-	currentState VARCHAR(2),
-	phoneNumber VARCHAR(10) NOT NULL,
+	email VARCHAR(35) NOT NULL,
+	currentState CHAR(2),
+	phoneNumber CHAR(10) NOT NULL,
+	phoneNetwork VARCHAR(20) NOT NULL,
 	CONSTRAINT student_primarykey_usernamepk PRIMARY KEY(userName)
 );
 
 CREATE TABLE LinkedU.recruiter
 (
-	firstName VARCHAR2(20)NOT NULL,
-	lastName VARCHAR2(20) NOT NULL,
-	userName VARCHAR2(10) NOT NULL,
-	university VARCHAR2(50) NOT NULL,
+	firstName VARCHAR(20)NOT NULL,
+	lastName VARCHAR(20) NOT NULL,
+	userName VARCHAR(10) NOT NULL,
+	university VARCHAR(50) NOT NULL,
 	accountStatus BOOLEAN default false,
-	email VARCHAR2(35) NOT NULL,
-	phoneNumber VARCHAR(10) NOT NULL,
+	email VARCHAR(35) NOT NULL,
+	phoneNumber CHAR(10) NOT NULL,
 	CONSTRAINT recruiter_primarykey_usernamepk PRIMARY KEY(userName)
 );
 
 CREATE TABLE LinkedU.Major
 (
 	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-	name VARCHAR2(50) NOT NULL,
+	majorName VARCHAR(50) NOT NULL,
 	CONSTRAINT primary_key_MajorID PRIMARY KEY (id)
 );
 
 CREATE TABLE LinkedU.University
 (
 	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-	name VARCHAR2(50) NOT NULL,
-	USState VARCHAR(2) NOT NULL,
-	picture VARCHAR2(40),
-	bio VARCHAR2(4000)
+	universityName VARCHAR(50) NOT NULL,
+	USState CHAR(2) NOT NULL,
+	picture VARCHAR(40),
+	bio VARCHAR(4000)
 	idealGPA DOUBLE,
 	idealACT INTEGER,
 	idealSAT INTEGER,
-	websiteLink VARCHAR2(150),
-	applicationLink VARCHAR2(160),
+	websiteLink VARCHAR(150),
+	applicationLink VARCHAR(160),
 	highlighted BOOLEAN default false,
-	notableMajors VARCHAR2 (50),
+	notableMajors VARCHAR (50),
 	timeSubscribed INTEGER default 0,
-	CONSTRAINT primary_key_MajorID PRIMARY KEY (id),
 	CONSTRAINT primary_key_universityName PRIMARY KEY (name)
 );
 
 CREATE TABLE LinkedU.Login
 (
-	userName VARCHAR2(20),
-	password VARCHAR2(200)NOT NULL,
-	accountType VARCHAR2(9) NOT NULL,
+	userName VARCHAR(20),
+	password VARCHAR(200)NOT NULL,
+	accountType VARCHAR(9) NOT NULL,
 	CONSTRAINT linkedULogin_primarykey_usernamepk PRIMARY KEY(userName)
 );
 
@@ -85,55 +85,55 @@ VALUES ('recruiter1', 'password1', 'recruiter');
 INSERT VALUES INTO LinkedU.Login (userName, password, accountType)
 VALUES ('recruiter2', 'password2', 'recruiter');
 
-INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, highSchool, currentUniversity, ACTScores, SATScores, GPA, GraduationDate, currentState, email, phoneNumber)
-VALUES ('student1', 'test', 'student1', 'a generically named high school',NULL ,28, NULL, 2.5, '2013-05-28', 'IL','bknigh1@ilstu.edu','6309346236');
+INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, highSchool, currentUniversity, ACTScores, SATScores, GPA, GraduationDate, currentState, email, phoneNumber, phoneNetwork)
+VALUES ('student1', 'test', 'student1', 'a generically named high school',NULL ,28, NULL, 2.5, '2013-05-28', 'IL','bknigh1@ilstu.edu','6309346236', 'Google');
 
-INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, highSchool, currentUniversity, ACTScores, SATScores, GPA, GraduationDate, currentState, email, phoneNumber)
-VALUES ('student2', 'test', 'student2', 'a differently named high school','a college that exists',32, NULL, 4.0, '2017-05-28', 'IL','bknigh1@ilstu.edu','6309346236');
+INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, highSchool, currentUniversity, ACTScores, SATScores, GPA, GraduationDate, currentState, email, phoneNumber, phoneNetwork)
+VALUES ('student2', 'test', 'student2', 'a differently named high school','a college that exists',32, NULL, 4.0, '2017-05-28', 'IL','bknigh1@ilstu.edu','6309346236', 'Google');
 
-INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, highSchool, currentUniversity, ACTScores, SATScores, GPA, GraduationDate, currentState, email, phoneNumber, accountStatus)
-VALUES ('student3', 'test', 'student3', 'another different named high school',NULL,NULL, 400, 3.5, '2018-05-28', 'IL','bknigh1@ilstu.edu','6309346236',true);
+INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, highSchool, currentUniversity, ACTScores, SATScores, GPA, GraduationDate, currentState, email, phoneNumber,phoneNetwork, accountStatus)
+VALUES ('student3', 'test', 'student3', 'another different named high school',NULL,NULL, 400, 3.5, '2018-05-28', 'IL','bknigh1@ilstu.edu','6309346236','Google',true);
 
-INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, university, email, phoneNumber)
-VALUES ('recruiter1', 'test', 'recruiter1', 'Illinois State University', 'bknigh1@ilstu.edu','6309346236');
+INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, university, email, phoneNumber, phoneNetwork)
+VALUES ('recruiter1', 'test', 'recruiter1', 'Illinois State University', 'bknigh1@ilstu.edu','6309346236', 'Google');
 
-INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, university, email, phoneNumber, accountStatus)
-VALUES ('recruiter2', 'test', 'recruiter1', 'University of Illinois', 'bknigh1@ilstu.edu','6309346236', true);
+INSERT VALUES INTO LinkedU.Student (userName, firstName, lastName, university, email, phoneNumber, accountStatus, phoneNetwork)
+VALUES ('recruiter2', 'test', 'recruiter1', 'University of Illinois', 'bknigh1@ilstu.edu','6309346236', true, 'Google');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Mathematics');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Engineering');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Teaching');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Physics');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Computer Science');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Nursing');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Biology');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Chemistry');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Business');
 
-INSERT VALUES INTO LinkedU.Major (name)
+INSERT VALUES INTO LinkedU.Major (majorName)
 VALUES ('Law');
 
-INSERT VALUES INTO LinkedU.University (name, USState, picture, bio, idealGPA,idealACT, idealSAT, websiteLink, applicationLink,highlighted,notableMajors, timeSubscribed)
+INSERT VALUES INTO LinkedU.University (universityName, USState, picture, bio, idealGPA,idealACT, idealSAT, websiteLink, applicationLink,highlighted,notableMajors, timeSubscribed)
 VALUES ('University of Illinois', 'IL', NULL,'This is a pregenerated statement about a university. Typically this includes the location and contact information of the university and a short statement',
 3.32,26,1340,'http://illinois.edu/','http://admissions.illinois.edu/apply/' , false, '2,1,5,8',0);
 
-INSERT VALUES INTO LinkedU.University (name, USState, picture, bio, idealGPA,idealACT, idealSAT, websiteLink, applicationLink,highlighted,notableMajors, timeSubscribed)
+INSERT VALUES INTO LinkedU.University (universityName, USState, picture, bio, idealGPA,idealACT, idealSAT, websiteLink, applicationLink,highlighted,notableMajors, timeSubscribed)
 VALUES ('Illinois State University', 'IL', NULL,'This is a pregenerated statement about a university. Typically this includes the location and contact information of the university and a short statement',
 2.37,22,NULL,'https://illinoisstate.edu/','http://admissions.illinoisstate.edu/apply/' , true, '3,1,5,6,9',0);
