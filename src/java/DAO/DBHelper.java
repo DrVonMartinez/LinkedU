617c34af0a2d0db09a72a6c9b3f76c3066f1044e
IT353S734 <IT353S734@WEBDEV-POOL-31.ad.ilstu.edu>
2017-04-25 22:22:18 -0500

    DBHelper filled

--- src/java/DAO/DBHelper.java
+++ src/java/DAO/DBHelper.java
@@ -5,10 +5,39 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author it353s723
  */
 public class DBHelper {
-    
+
+    /**
+     * Creates a new instance of DBHelper
+     */
+    public DBHelper() {
+    }
+
+    public static void loadDriver(String driverStr) {
+        try {
+            Class.forName(driverStr);
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static Connection connect2DB(String connectStr, String userName, String password) {
+
+        String myDB = connectStr;
+        Connection DBConn = null;
+        try {
+            DBConn = DriverManager.getConnection(myDB, userName, password);
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return DBConn;
+    }
 }
