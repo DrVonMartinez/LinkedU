8e1cd095da69c85bd93454dd0c10fa882c83ef37
it353s728 <it353s728@WEBDEV-POOL-43.ad.ilstu.edu>
2017-04-30 20:43:57 -0500

    Comment giving credit to Ozeki for message sender

--- src/java/Model/MessageSender.java
+++ src/java/Model/MessageSender.java
@@ -1,17 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Messager service creation credit to Ozeki 
  */
 package Model;
 import java.io.*;
 import java.util.*;
 import hu.ozeki.*;
 
-/**
- *
- * @author it353s728
- */
 public class MessageSender extends OzSmsClient{
 
         public MessageSender(String host, int port) throws IOException, InterruptedException {
